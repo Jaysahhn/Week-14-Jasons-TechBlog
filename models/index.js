8b54788899b9b08946a06952abf63d977c18ef94
Jaysahhn <jasonmason96@yahoo.com>
2023-11-30 20:54:16 -0600

    added file structure to models/index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,25 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Post = require('./Post');
 
+// Define associations
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+});
 
-//todo add relations between user and blog.
+Blog.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Blog.hasMany(Post, {
+    foreignKey: 'blog_id',
+});
+
+Post.belongsTo(Blog, {
+    foreignKey: 'blog_id',
+});
+
+module.exports = { User, Blog, Post };
 
 
 module.exports = { User, Blog };
